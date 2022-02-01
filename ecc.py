d0511fb8de0fc3e43cda9ea7918ae97792f9d880
enigbe <ochekliyeenigbe@gmail.com>
2022-02-01 14:39:51 +0100

    feat: add not equal operator to FieldElement
    - override the != operator on FieldElement
    - add explanatory docstrings to methods
    
    [WIP]

--- ecc.py
+++ ecc.py
@@ -4,8 +4,17 @@ Library containing helper classes for elliptic curve cryptography (ECC)
 
 class FieldElement:
     """An element belonging to the finite set"""
-    def __init__(self, num: int, prime: int) -> None:
-        """initialize a FiniteElement"""
+    def __init__(self, num: int, prime: int) -> "FieldElement":
+        """
+        Initializes a FiniteElement
+        
+        Args: 
+            num (int): a number of integer type, e.g. 5
+            prime (int): a prime number e.g 7
+
+        Returns:
+            FieldElement_7(5)
+        """
         if num >= prime or num < 0:
             error = f'Num {num} not in field range 0 to {prime - 1}'
             raise ValueError(error)
@@ -17,8 +26,30 @@ class FieldElement:
         return f'FieldElement_{self.prime}({self.num})'
 
     def __eq__(self, other: "FieldElement") -> bool:
-        """Equality operator for objects of type FieldElement"""
+        """
+        Equality operator for objects of type FieldElement.
+        Checks if the passed in element is equal to the instantiated element
+
+        Args:
+            other (FieldElement): e.g. FieldElement_13(7)
+
+        Returns:
+            bool: True if equal, False otherwise
+        """
         if other is None:
             return False
 
         return self.num == other.num and self.prime == other.prime
+
+    def __ne__(self, other: "FieldElement") -> bool:
+        """
+        Not equality operator for objects of type FieldElement.
+        Checks if the passed in element is not equal to the instantiated element
+
+        Args:
+            other (FieldElement): e.g. FieldElement_13(7)
+
+        Returns:
+            bool: True if not equal, False otherwise
+        """
+        return not (self == other)
