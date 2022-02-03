7a524f8370fd7395b20cebe6fd97d1cdca633035
enigbe <ochekliyeenigbe@gmail.com>
2022-02-03 23:06:05 +0100

    feat: elliptic curve point addition
    - implement EC point addition for when both points
    cut through the EC at 3 and 1 points
    
    [Delivers]

--- ecc.py
+++ ecc.py
@@ -3,7 +3,7 @@ Library containing helper classes for elliptic curve cryptography (ECC)
 """
 
 class FieldElement:
-    """An element belonging to the finite set"""
+    """An element belonging to a finite set"""
     def __init__(self, num: int, prime: int) -> "FieldElement":
         """
         Initializes a FiniteElement
@@ -134,3 +134,86 @@ class FieldElement:
 
         num = (self.num * pow(other.num, self.prime - 2, self.prime)) % self.prime
         return self.__class__(num, self.prime)
+
+
+class Point:
+    """
+    A point on an elliptic curve
+    """
+    def __init__(self, x: int, y: int, a: int, b: int) -> "Point":
+        """
+        Instantiates a point
+
+        Args:
+            x (int): x-coordinate
+            y (int): y-coordinate 
+            a (int): constant coefficient of x
+            b (int): constant
+
+        """
+        self.a = a
+        self.b = b
+        self.x = x
+        self.y = y
+
+        # Point at Infinity
+        if self.x is None and self.y is None:
+            return
+
+        if (self.y ** 2) != (self.x ** 3) + (a * x) + b:
+            raise ValueError(f'({x}, {y}) is not on the curve')
+
+    def __eq__(self, other: "Point") -> bool:
+        """
+        Overrides the equality operator '==' to check if two points are equal
+
+        Args:
+            other (Point): e.g. Point(-1, -1, 5, 7) 
+
+        Returns:
+            boolean: True if equal, false otherwise
+        """
+        return self.x == other.x and self.y == other.y and self.a == other.a and self.b == other.b
+
+
+    def __ne__(self, other: "Point") -> bool:
+        """
+        Checks if two points are not equal
+        """
+        return not (self == other)
+
+    def __add__(self, other: "Point") -> "Point":
+        """
+        Adds one point to another
+        """
+        if self.a != other.a or self.b != other.b:
+            raise TypeError(f'Points {self}, {other} are not on the same curve')
+
+        if self.x is None:
+            return other
+
+        if other.x is None:
+            return self
+
+        # Vertical line 
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
+
+        # P1(x,y) != P2(x,y)
+        if self.x != other.x:
+            slope = (other.y - self.y) / (other.x - self.x)
+            x = pow(slope, 2) - self.x - other.x
+            y = slope*(self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+        # Both points at the same spot
+        if self == other:
+            slope = (3 * pow(self.x, 2) + self.a) / (2 * self.y)
+            x = pow(slope, 2) - (2 * self.x)
+            y = slope*(self.x - x) - self.y
+            return self.__class__(x, y, self.a, self.b)
+
+        # P1 == P2 and y coordinate is 0
+        if self == other and self.y == 0 * self.x:
+            return self.__class__(None, None, self.a, self.b)
+
