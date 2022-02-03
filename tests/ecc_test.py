2410b59a1e4a2e22bed3ca932387a908c9c96227
enigbe <ochekliyeenigbe@gmail.com>
2022-02-04 00:06:15 +0100

    test: setup preliminary testing with pytest

---
+++ tests/ecc_test.py
@@ -0,0 +1,32 @@
+"""
+Tests the classes in ecc.py
+"""
+
+import pytest
+from ecc import FieldElement, Point
+
+def test_on_curve():
+    prime = 223
+    a = FieldElement(0, prime)
+    b = FieldElement(7, prime)
+
+    valid_points = (
+        (192, 105),
+        (17, 56),
+        (1, 193)
+    )
+    invalid_points = (
+        (200, 119),
+        (42, 99)
+    )
+
+    for x_raw, y_raw in valid_points:
+        x = FieldElement(x_raw, prime)
+        y = FieldElement(y_raw, prime)
+        Point(x, y, a, b)
+
+    for x_raw, y_raw in invalid_points:
+        x = FieldElement(x_raw, prime)
+        y = FieldElement(y_raw, prime)
+        with pytest.raises(ValueError):
+            Point(x, y, a, b)
